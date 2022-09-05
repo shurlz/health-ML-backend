27448df7617c902bd2f1139f0ec7800e6e0390c7
shurlz <johnegbeola10@gmail.com>
2022-09-05 22:05:01 +0100

    static adjustment

--- mlclinic/settings.py
+++ mlclinic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h0702cjbys97zil^babzpxj-lyjem5jf#8r!!&dwifpmerab@+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
